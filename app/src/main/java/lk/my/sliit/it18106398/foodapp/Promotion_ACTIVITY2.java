7f11e8310874622e5874682b7ad6801cfbdd8310
Prabha97 <prabhashwari.lk@gmail.com>
2019-08-06 13:13:36 +0530

    created listview to promotions list

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/Promotion_ACTIVITY2.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/Promotion_ACTIVITY2.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.time.Instant;
 import java.util.ArrayList;
 
 public class Promotion_ACTIVITY2 extends AppCompatActivity {
@@ -14,6 +17,7 @@ public class Promotion_ACTIVITY2 extends AppCompatActivity {
    // ListView listView;
 //    String names = {name1, name2};
 //    Integer[] images ={I1,I2};
+    Button btn_promo1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,19 @@ public class Promotion_ACTIVITY2 extends AppCompatActivity {
        // names = findViewById(R.id.txtView1);
 
         //final ArrayList<String>arrayList
+        btn_promo1 = (Button) findViewById(R.id.buttonPromo1);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        btn_promo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ins = new Intent(Promotion_ACTIVITY2.this,Promotion_ACTIVITY3.class);
+                startActivity(ins);
+            }
+        });
     }
 }
---
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/PromotionsAdapter.java
@@ -0,0 +1,66 @@
+package lk.my.sliit.it18106398.foodapp;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class PromotionsAdapter extends RecyclerView.Adapter<PromotionsAdapter.myViewHolder> {
+    private Context mContext;
+    private ArrayList<ModelPromotions> mList;
+
+    public PromotionsAdapter(Context mContext, ArrayList<ModelPromotions> mList) {
+        this.mContext = mContext;
+        this.mList = mList;
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View v = inflater.inflate(R.layout.list_promotions, parent, false);
+
+        myViewHolder viewHolder = new myViewHolder(v);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+        ModelPromotions promotions = mList.get(position);
+
+        ImageView promoImg = holder.promo_img;
+        TextView promoName = holder.promo_txt;
+
+        promoImg.setImageResource(mList.get(position).getPromoImage());
+        promoName.setText(promotions.getPromoName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public class myViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView promo_img;
+        TextView promo_txt;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            promo_img = itemView.findViewById(R.id.promoImage);
+            promo_txt = itemView.findViewById(R.id.promoText);
+        }
+    }
+}
