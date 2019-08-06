80479a1c99d0ec368a65dc6b6ea93402689b2b19
TOshadeeG96 <oshadeekaps@gmail.com>
2019-08-06 16:41:20 +0530

    create food home interface

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/RecycleViewFood.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/RecycleViewFood.java
@@ -10,5 +10,6 @@ public class RecycleViewFood extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle_view_food);
+
     }
 }
