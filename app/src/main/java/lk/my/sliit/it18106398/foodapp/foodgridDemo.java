7c42cb2de0cd4878bc845b2dcc8e5106577ea533
TOshadeeG96 <oshadeekaps@gmail.com>
2019-08-06 12:42:54 +0530

    customer home

---
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/foodgridDemo.java
@@ -0,0 +1,14 @@
+package lk.my.sliit.it18106398.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class foodgridDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foodgrid_demo);
+    }
+}
