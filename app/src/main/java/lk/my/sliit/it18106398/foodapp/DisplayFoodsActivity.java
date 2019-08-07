539eb1ecc0a40052a6166a924c43af86482390bb
TOshadeeG96 <oshadeekaps@gmail.com>
2019-08-07 17:01:58 +0530

    changed names in Food List

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/DisplayFoodsActivity.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/DisplayFoodsActivity.java
@@ -1,6 +1,9 @@
 package lk.my.sliit.it18106398.foodapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,17 +13,37 @@ public class DisplayFoodsActivity extends AppCompatActivity {
     ImageView imageView;
     TextView textView;
 
+    Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_foods);
 
-
+        b1 = findViewById(R.id.btn1);
 
         imageView = findViewById(R.id.imageTop);
         textView = findViewById(R.id.foodNameTxtView);
 
         imageView.setImageResource(getIntent().getIntExtra("image_id", 00));
         textView.setText(getIntent().getStringExtra("res_name"));
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(DisplayFoodsActivity.this, MyBag.class);
+                startActivity(intent);
+            }
+
+
+        });
     }
 }
