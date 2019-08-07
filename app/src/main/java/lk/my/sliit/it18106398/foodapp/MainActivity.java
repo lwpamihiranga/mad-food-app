1118b71ba21f3ceafda0dddd17a3f7b493fb1f38
Amith Mihiranga <lwpamihiranga@gmail.com>
2019-08-07 17:40:07 +0530

    navigation drawer created from drawer activity

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/MainActivity.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends AppCompatActivity{
         b5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, NavigationDrawerActivity.class);
                 startActivity(intent);
             }
 
