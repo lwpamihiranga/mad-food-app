4a7f3beba845071502206da26b895c2bc01499c0
Amith Mihiranga <lwpamihiranga@gmail.com>
2019-08-08 08:07:44 +0530

    debugging delivery person view not showing by adapter

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/ListDeliveryPersonAdapter.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/ListDeliveryPersonAdapter.java
@@ -1,11 +1,13 @@
 package lk.my.sliit.it18106398.foodapp;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,6 +28,7 @@ public class ListDeliveryPersonAdapter extends RecyclerView.Adapter<ListDelivery
     @Override
     public PersonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(mContext);
+        Log.d("message", "onBindCalled");
         View v = inflater.inflate(R.layout.delivery_person, parent, false);
 
         return new PersonViewHolder(v);
@@ -39,7 +42,6 @@ public class ListDeliveryPersonAdapter extends RecyclerView.Adapter<ListDelivery
         TextView personName = holder.personName;
 
         personImage.setImageResource(mList.get(position).getPersonImage());
-
         personName.setText(modelDeliverPerson.getPersonName());
     }
 
