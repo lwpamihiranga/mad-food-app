b4f6653a9b39503c60efe472d674d1777fee513d
Amith Mihiranga <lwpamihiranga@gmail.com>
2019-08-08 06:53:54 +0530

    list person recycle view completed

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/DeliveryPersonList.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/DeliveryPersonList.java
@@ -1,6 +1,7 @@
 package lk.my.sliit.it18106398.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -21,6 +22,18 @@ public class DeliveryPersonList extends AppCompatActivity {
 
         deliverPeople = new ArrayList<>();
 
-        deliverPeople.add(new ModelDeliverPerson(R.))
+        deliverPeople.add(new ModelDeliverPerson(R.drawable.pro1, "Anjalie Jolly"));
+        deliverPeople.add(new ModelDeliverPerson(R.drawable.pro2, "Andrew Michel"));
+        deliverPeople.add(new ModelDeliverPerson(R.drawable.pro3, "Sadun Sathsara"));
+        deliverPeople.add(new ModelDeliverPerson(R.drawable.pro4, "Kate Perry"));
+        deliverPeople.add(new ModelDeliverPerson(R.drawable.pro5, "Emma Watson"));
+        deliverPeople.add(new ModelDeliverPerson(R.drawable.pro6, "Ellen Dengerous"));
+        deliverPeople.add(new ModelDeliverPerson(R.drawable.pro7, "Eric Chen"));
+        deliverPeople.add(new ModelDeliverPerson(R.drawable.pro8, "Shawn Mendis"));
+
+        ListDeliveryPersonAdapter adapter = new ListDeliveryPersonAdapter(this, deliverPeople);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
