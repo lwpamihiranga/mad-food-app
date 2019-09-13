5cfb6d53789149ac1b085aa2c8c0d0f0bd152723
Amith Mihiranga <lwpamihiranga@gmail.com>
2019-09-13 18:38:55 +0530

    login completed partially for firebase to identify user type

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/LoginActivity.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package lk.my.sliit.it18106398.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,8 +11,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -53,7 +57,31 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dbRef = FirebaseDatabase.getInstance().getReference().child("User");
+                dbRef = FirebaseDatabase.getInstance().getReference().child("User").child(editTextUsername.getText().toString().trim());
+
+                dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChildren()) {
+                            String username = dataSnapshot.child("email").getValue().toString();
+                            String password = dataSnapshot.child("password").getValue().toString();
+                            String type = dataSnapshot.child("type").getValue().toString();
+
+                            if(type.equalsIgnoreCase("Customer")) {
+                                Toast.makeText(getApplicationContext(), "Singing in as a Customer", Toast.LENGTH_SHORT).show();
+                            } else if(type.equalsIgnoreCase("Restaurant")) {
+                                Toast.makeText(getApplicationContext(), "Singing in as a Restaurant", Toast.LENGTH_SHORT).show();
+                            } else if(type.equalsIgnoreCase("Delivery")) {
+                                Toast.makeText(getApplicationContext(), "Singing in as a Delivery Driver", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
     }
