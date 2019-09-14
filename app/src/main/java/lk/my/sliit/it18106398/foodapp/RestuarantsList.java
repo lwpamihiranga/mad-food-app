e4c0c1f38d821d82865de921e55f9a62966d4777
Amith Mihiranga <lwpamihiranga@gmail.com>
2019-09-14 18:43:45 +0530

    res list test

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/RestuarantsList.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/RestuarantsList.java
@@ -1,10 +1,24 @@
 package lk.my.sliit.it18106398.foodapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
@@ -40,8 +54,5 @@ public class RestuarantsList extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
-
-
-
     }
 
