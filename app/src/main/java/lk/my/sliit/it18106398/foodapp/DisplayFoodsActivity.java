d8edc17801606e855c314b560c2219975ceb6d8c
Saumya <saumya19970722@gmail.com>
2019-09-17 17:41:27 +0530

    order class added

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/DisplayFoodsActivity.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/DisplayFoodsActivity.java
@@ -4,43 +4,64 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DisplayFoodsActivity extends AppCompatActivity {
     ImageView imageView;
-    TextView textView;
+    TextView description;
+    EditText qty;
 
     Button b1;
+    DatabaseReference dbRef;
+    Order o;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_foods);
 
+        description = findViewById(R.id.foodNameTxtView);
+        qty = findViewById(R.id.qty);
+
         b1 = findViewById(R.id.btn1);
+
+        o = new Order();
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
                 Intent intent1 = new Intent(DisplayFoodsActivity.this, MyBag.class);
                 startActivity(intent1);
+
+                dbRef = FirebaseDatabase.getInstance().getReference().child("Order");
+                try{
+                    o.setQty(Integer.parseInt(qty.getText().toString().trim()));
+                    o.setDescription(description.getText().toString().trim());
+
+                    dbRef.push().setValue(o);
+
+                    Toast.makeText(getApplicationContext(),"Data saved successfully",Toast.LENGTH_SHORT).show();
+                    qty.setText("");
+                }
+                catch (NumberFormatException e){
+                    Toast.makeText(getApplicationContext(),"Invalid quantity",Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
-
-
         imageView = findViewById(R.id.imageTop);
-        textView = findViewById(R.id.foodNameTxtView);
 
         imageView.setImageResource(getIntent().getIntExtra("image_id", 00));
-        textView.setText(getIntent().getStringExtra("res_name"));
-
-
+        description.setText(getIntent().getStringExtra("res_name"));
     }
-
-
-    
 }
---
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/Order.java
@@ -0,0 +1,25 @@
+package lk.my.sliit.it18106398.foodapp;
+
+public class Order {
+    private Integer qty;
+    private String description;
+
+    public Order() {
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
