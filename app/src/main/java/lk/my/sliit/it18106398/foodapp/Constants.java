0a4a8a6d6f35b4dd62233e36b7e53bb47248c4de
Saumya <saumya19970722@gmail.com>
2019-09-21 10:51:56 +0530

    Orders class added

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/Constants.java
+++
@@ -1,6 +0,0 @@
-package lk.my.sliit.it18106398.foodapp;
-
-public class Constants {
-    public static final String STORAGE_PATH_UPLOADS = "uploads/";
-    public static final String DATABASE_PATH_UPLOADS = "uploads";
-}
--- app/src/main/java/lk/my/sliit/it18106398/foodapp/MyBag.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/MyBag.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
