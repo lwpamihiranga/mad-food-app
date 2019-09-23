31f1119aaa606c151f7c9cef787395b203dd62ad
TOshadeeG96 <oshadeekaps@gmail.com>
2019-09-23 22:16:29 +0530

    Done some changes in FoodTable Class

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/FoodTable.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/FoodTable.java
@@ -2,33 +2,18 @@ package lk.my.sliit.it18106398.foodapp;
 
 public class FoodTable {
 
-    public String name;
-    public String ItemNo;
-
-
-    public FoodTable(String name, String itemNo) {
-        this.name = name;
-        ItemNo = itemNo;
-    }
+    private String name;
+    private String ItemNo;
 
     public FoodTable() {
 
-
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() {return name;}
 
-    public String getItemNo() {
-        return ItemNo;
-    }
+    public String getItemNo() { return ItemNo;}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public void setItemNo(String itemNo) {
-        ItemNo = itemNo;
-    }
+    public void setItemNo(String itemNo) { ItemNo = itemNo;}
 }
