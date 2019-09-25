1276a0d46cab4088a66cf12c8cda7b4387cfac5a
Prabha97 <prabhashwari.lk@gmail.com>
2019-09-25 19:45:57 +0530

    changes on Adapter class

--- app/src/main/java/lk/my/sliit/it18106398/foodapp/ResViewPromotionAdapter.java
+++ app/src/main/java/lk/my/sliit/it18106398/foodapp/ResViewPromotionAdapter.java
@@ -9,9 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
